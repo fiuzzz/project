9c7e4aa6d90f5e90c18d66958e06703e4935433f
fiuzzz <faizramdhn05@gmail.com>
2025-02-08 10:33:32 +0700

    menemabahkan gambar sampul

--- projek/app/api/v1/comic/controller.js
+++ projek/app/api/v1/comic/controller.js
@@ -1,7 +1,6 @@
 import { comics,genre } from "./model.js"
 const getAll = (req, res) => {
-    // res.render("template/index", {comics})
-    res.json({comics,genre})
+    res.render("template/index", {comics,genre})
 }
 
 
